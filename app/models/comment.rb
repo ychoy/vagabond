62b51097a1a6219a0440d2e88825e8917a9fda4b
Yan Yin Choy <yanyinchoy@gmail.com>
2017-03-16 02:29:47 -0700

    validations and model relationships with tip and user

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,5 @@
 class Comment < ApplicationRecord
+  belongs_to :tip
+  belongs_to :user
+  validates :content, presence: true, length: { in: 1..200,  message: "Comment cannot be blank. Share your thoughts!"}, allow_nil: false
 end
