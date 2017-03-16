e9ec6cfba7acd75f56c1445f35c17aa4ab2143c2
Yan Yin Choy <yanyinchoy@gmail.com>
2017-03-15 19:29:58 -0700

    updated models

--- app/models/city.rb
+++ app/models/city.rb
@@ -2,5 +2,5 @@ class City < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50 }
   validates :photo, presence: true
 
-  has_many :tips
+  has_many :tips, dependent: :destroy 
 end
