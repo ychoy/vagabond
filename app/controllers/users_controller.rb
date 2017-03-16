7c2a123b026c597cd6668039a9cd3ff0c82de429
cnwilliams <hello.christenwilliams@gmail.com>
2017-03-15 20:19:48 -0700

    allows user to edit profile img

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,7 +40,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :current_city, :password)
+    params.require(:user).permit(:first_name, :last_name, :email, :current_city, :password, :profile_image)
   end
 
   def set_user
