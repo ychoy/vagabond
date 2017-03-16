af2579ffb178d3e0bf153dee0c571c71831b307a
cnwilliams <hello.christenwilliams@gmail.com>
2017-03-15 19:14:39 -0700

    titleize on user show, tips

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,14 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:edit, :show, :update]
   before_action :authorize_user, only: [:edit, :update]
 
-  # index will likely be moved
+
   def index
     @users = User.all
   end
 
   def show
+    # @city = City.all
+    @tips = @user.tips
   end
 
   def new
