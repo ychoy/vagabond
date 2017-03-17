84fc317d20214dcf729813999a70090799682b8f
Yan Yin Choy <yanyinchoy@gmail.com>
2017-03-17 14:40:16 -0700

    updated comments view, only comment creator can delete their comment. tip view

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,17 @@
 class CommentsController < ApplicationController
   before_action :set_tip, only: [:create, :destroy]
-  before_action :require_login, only: [:create, :destroy]
+  before_action :require_login, only: [:new, :create, :destroy]
+
+  def new
+    @comment = Comment.new()
+  end
 
   def create
     @comment = @tip.comments.create(comment_params)
     @comment.user_id = current_user.id
+    current_user.comments << @comment
     if @comment.save
       redirect_to tip_path(@tip)
-    else
-      flash[:error] = @comment.errors.full_messages.join(", ")
     end
 
   end
@@ -17,18 +20,19 @@ class CommentsController < ApplicationController
     @comment = @tip.comments.find(params[:id])
     if @comment.destroy
       redirect_to tip_path(@tip)
-    else
-      flash[:error] = @comment.errors.full_messages.join(", ")
-      redirect_to tip_path(@tip)
     end
   end
 
   private
     def comment_params
-      params.require(:comment).permit(:content, :user_id, :post_id)
+      params.require(:comment).permit(:content, :user_id, :tip_id)
     end
 
     def set_tip
       @tip = Tip.find(params[:tip_id])
+    end
+
+    def set_comment
+      @comment = Comment.find_by_id(params[:id])
     end
 end
